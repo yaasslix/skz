cdd899d7be689211e5fb26fe5347c11e3ab5149c
yasminefadiss <127844757+yasminefadiss@users.noreply.github.com>
2024-01-06 21:48:55 +0100

    final final

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ function openTab(evt, tabName) {
         tabcontent[i].style.display = "none";
     }
 
-    // Mostrar el contenido de la pestaña actual
+    // Eliminar la clase "active" de todos los elementos con class="tablinks"
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Mostrar el contenido de la pestaña actual y añadir una clase "active" al botón que abrió la pestaña
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+// Función para mostrar la categoría de photocards seleccionada
+function showPhotocardCategory(category) {
+    // Ocultar todas las categorías
+    var categories = document.getElementsByClassName("album-category");
+    for (var i = 0; i < categories.length; i++) {
+        categories[i].style.display = "none";
+    }
+
+    // Mostrar la categoría seleccionada
+    var selectedCategory = document.getElementById(category);
+    if (selectedCategory) {
+        selectedCategory.style.display = "block";
+    }
 }
 
 // Agregar un evento de 'click' al primer botón de pestaña para abrirlo por defecto al cargar la página
